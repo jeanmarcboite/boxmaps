f72c7d94ac68989999ebe5591f259fca2ef33c6d
Jean-Marc Boite <jeanmarcboite@gmail.com>
2018-05-01 08:46:52 +0200

    use vuex-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import createPersistedState from 'vuex-persistedstate'
 import createLogger from 'vuex/dist/logger'
 import settings from './modules/settings'
 import layers from './modules/layers'
+import VuexPersist from 'vuex-persist'
+
+const vuexStorage = new VuexPersist({
+  key: 'maps-vuex', // The key to store the state on in the storage provider.
+  storage: window.localStorage, // or window.sessionStorage or localForage
+  // Function that passes the state and returns the state with only the objects you want to store.
+  // reducer: state => state,
+  // Function that passes a mutation and lets you decide if it should update the state in localStorage.
+  // filter: mutation => (true)
+})
 
 Vue.use(Vuex)
 const debug = process.env.NODE_ENV !== 'production'
@@ -12,7 +21,7 @@ const state = {
   app: 'Vuetify Map'
 }
 
-const plugins = [createPersistedState({key: 'maps-vuex'})]
+const plugins = [vuexStorage.plugin]
 if (debug) {
   plugins.push(createLogger())
 }
